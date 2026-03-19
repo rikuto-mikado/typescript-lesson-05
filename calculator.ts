7a21f79c0240ddc751713946902b45890f8ce470
Rikuto <mikadorikuto@gmail.com>
2026-03-19 17:56:00 +0900

    start new lecture

---
+++ calculator.ts
@@ -0,0 +1,17 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function caclulateInvestment(data) {
+
+}
+
+function printResults(result) {
+
+}
+
+const results = caclulateInvestment()
+
+printResults(results);
